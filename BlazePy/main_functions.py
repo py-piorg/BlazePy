85a07532d5ff155b1d50c70bde98ac1230fabf9c
KwabenaAddai <>
2021-08-03 22:19:24 +0000

    Initial commit

---
+++ BlazePy/main_functions.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Console:
+  def __init__(self):
+    x = self.x
+  def show(x):
+    x == ""    
+    print(x)
+
+  def MakeWindow(x, y):
+    pygame.init()
+    pygame.display.set_mode((x, y), pygame.RESIZABLE)
+    pygame.display.update()
+
+  class GUI:
+    def __init__(self)
+
+Console.show("Hello World")
+Console.MakeWindow(200, 200)
